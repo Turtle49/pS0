ff178e6ea5b4e8646194e19def7b4c09248d4928
Turtle49 <48773268+Turtle49@users.noreply.github.com>
2023-06-20 16:55:36 -0400

    Update pScode-v2.py
    Full commit

--- pScode-v2.py
+++ pScode-v2.py
@@ -26,9 +26,32 @@ import supervisor
 #supervisor.disable_autoreload()
 supervisor.runtime.autoreload = False
 
-## Define Functions
-#Setup
+##Parameters##
+#Set valve timing parameters
+#Transit time, in seconds: time for a particle to travel from sensor to window of sorting
+transit = 0
+#Window time, in seconds: window of time a particle travels through the sorting junction
+window = .4; particle_arrival = 0 #create arrival timing variable
 
+#SDA Ports used by RGB sensor in tca multiplexer
+multi_port = [0, 2, 3]
+#Sensor integration time, in ms. 614.4/10 #for integration time 2.4 - 614.4 ms. Make sure the TCS34725 library adafruit_tcs34725.py has been modified, line 224
+RGB_int_time = 20
+#Gain of 1, 4, 16, 60
+RGB_gain = 4
+
+#Tolerance in color determination
+color_error = 1 #max angle, in radians, between data and reference
+
+##Sorting parameters
+base_weight = RGB_int_time/1000 #Data moving average; weight of new data for for moving average; inverse of number of points in moving average
+min_threshold = 10 #Minimum size of signal vectors, in bits, to be considered signal, not noise
+#Color reference list: [label, ID vector]
+data_ref = [['Blue', [-0.456,0.115,0.870]], ['Yellow', [0.716,0.631,0.297]],['Red', [0.782,-0.371,-0.238]],['Black', [-0.657,-0.568,-0.496]]]
+color_ref = data_ref[2][1]
+
+## Define Functions
+#CDC USB com Setup
 def node_setup():
     comm_device = usb_cdc.data#[1]
     return comm_device
@@ -43,73 +66,46 @@ def digitalRGB_sensor(i):
 
 #Check RGB vector to reference values
 #This function measures the angle between the 'vector' data and multiple reference 'vectors' in the list reference (e.g., RGB_ref) and returns the idex of the lowest value
-#def RGB_ref_check(data1,data2):
-#    refangles = []
-#    for ref in data1:
-#        refangles.append( math.acos( (ref[0]*data2[0] + ref[1]*data2[1] + ref[2]*data2[2]) / (.01 + math.pow((ref[0]**2 + ref[1]**2 + ref[2]**2),.5) * math.pow((data2[0]**2 + data2[1]**2 + data2[2]**2),.5)))  )
-#    return refangles.index(min(refangles))
-
-#Check RGB vector to reference values
-#This function measures the angle between the 'vector' data and multiple reference 'vectors' in the list reference (e.g., RGB_ref) and returns the idex of the lowest value
 def is_it_color(data,reference,error):
     angles = []
     for ref in reference:
-      angles.append(math.acos( (reference[0]*data[0] + reference[1]*data[1] + reference[2]*data[2]) / (.01 + math.pow((reference[0]**2 + reference[1]**2 + reference[2]**2),.5) * math.pow((data[0]**2 + data[1]**2 + data[2]**2),.5))))
+        angles.append(math.acos( (ref[0]*data[0] + ref[1]*data[1] + ref[2]*data[2]) / (.01 + math.pow((ref[0]**2 + ref[1]**2 + ref[2]**2),.5) * math.pow((data[0]**2 + data[1]**2 + data[2]**2),.5))))
     #now check if it's a sought color
-      if error >= min(angles):
-          result = True
-          #print('Particle!')
-      else:
-          result = False
-    return result
-
+    if error >= min(angles):
+        return True
+        #print('Particle!')
+    else:
+        return False
 
 #Modulus, or size of vector
-#This function works with any type of list, of any length. It does not worl with integers, float, and other non-lists
 def distance(a):
     d = 0
     for i in a:
         d += math.pow(i,2)
     return math.pow(d,.5)
 
-##Valve control
-#Set valve timing parameters
-#Transit time, in seconds: time for a particle to travel from sensor to window of sorting
-transit = 0#For noist pump 0.25. For centrigugal 0.1
-#Window time, in seconds: window of time a particle travels through the sorting junction
-window = .4
-particle_arrival = 0
 #Valve checking function
 def valve_control(current_time, arrival_time):
-    if (current_time - arrival_time > transit) and (current_time - arrival_time < transit + window):
+    if len(particle_time)>1 and (current_time - arrival_time[0] > transit) and (current_time - arrival_time[0] < transit + window):
         relay.value = relay_on
         #print('valve on!')
-    elif (current_time - arrival_time) > (transit + window):
+    elif (current_time - arrival_time[0]) > (transit + window):
         relay.value = not(relay_on)
-        particle_arrival = 0
-#return 0
+        #particle_arrival = 0
+        del arrival_time[0]
 
 ## Initialization of components
-
-#Initiate multi RGB sensors
-#Ports used by RGB sensor in tca multiplexer
-multi_port = [0, 2, 3]
-
 #Create the TCA9548A object and give it the I2C bus
 tca = adafruit_tca9548a.TCA9548A(board.I2C())
 time.sleep(.02)
 
 #Initiate RGB sensors
 sensorRGB = [0]*len(multi_port)
-RGB_int_time = 20 #sensor integration time, in ms. 614.4/10 #for integration time 2.4 - 614.4 ms. Make sure the TCS34725 library adafruit_tcs34725.py has been modified, line 224
 for i, port in enumerate(multi_port):
     sensorRGB[i] = mrs_tcs34725.TCS34725(tca[port]) ##WARNING: in Trinket M0 the UART object MUST be created before I2C https://learn.adafruit.com/circuitpython-essentials/circuitpython-uart-serial
     time.sleep(0.02)
     sensorRGB[i].integration_time = RGB_int_time #614.4/10 #for integration time 2.4 - 614.4 ms. Make sure the TCS34725 library adafruit_tcs34725.py has been modified, line 224
-    sensorRGB[i].gain = 4 #gain of 1, 4, 16, 60
-
-#Tolerance in color determination
-color_error = 1 #max angle, in radians, between data and reference
+    sensorRGB[i].gain = RGB_gain
 
 #Initiate CDC USB communication
 comm_device = node_setup() ##WARNING: in Trinket M0 the UART object MUST be created before I2C https://learn.adafruit.com/circuitpython-essentials/circuitpython-uart-serial
@@ -131,18 +127,7 @@ relay_on = True
 relay = digitalio.DigitalInOut(board.D13); relay.direction = digitalio.Direction.OUTPUT
 relay.value = relay_on; time.sleep(.3); relay.value = not(relay_on)
 
-##Sorting parameters
-base_weight = RGB_int_time/1000 #Data moving average; weight of new data for for moving average; inverse of number of points in moving average
-min_threshold = 10 #Minimum size of signal vectors, in bits, to be considered signal, not noise
-#Color reference list: [label, ID vector]
-data_ref = [['Blue', [-0.456,0.115,0.870]], ['Yellow', [0.716,0.631,0.297]],['Red', [0.782,-0.371,-0.238]],['Black', [-0.657,-0.568,-0.496]]]
-color_ref = data_ref[2][1]
-
-#Initilize particle buffer
-#particle_flag = False
-#particle_buffer = [0]
-
-##Acquire baselines
+##Initialize baselines
 data_base = [0,0,0,0]
 data_long_base = [0,0,0,0,0,0,0,0,0,0,0,0]
 #initialization counter to capture a baseline automatically
@@ -150,7 +135,7 @@ base_init = 0
 base_count = 0 #counter for holding baselines
 
 #Initializae time variable for valving
-particle_arrival = 0
+particle_arrival = []
 
 data_raw_multi = [0,0,0,0]*len(multi_port)
 data = [0,0,0,0]
@@ -183,7 +168,6 @@ while True:
         for j in range(len(data_raw_multi[i])):
             data_long.append(data_raw_multi[i][j])
 
-
     ##Data processing and Classification
     #Initialization: loop sets the baseline for the 1st time...it take3s a couple of reads to stabilize
     ###  DELETE??XXXXX
@@ -197,16 +181,15 @@ while True:
     #Discriminate small signals and double-counting slow particles
     if (distance(data_long[0:2],data_long_base[0:2]) > min_threshold) or (distance(data_long[4:6],data_long_base[4:6]) > min_threshold) or distance(data_long[8:10],data_long_base[8:10]) > min_threshold:
         #Comparison to reference values for identification
-        if  XXXHere I need to test against the angle of the walls and the angle of the particle of interest...everything else needs to go:#is_it_color(data,color_ref,color_error):
+        if  not(is_it_color(data_long[0:2],color_ref,color_error) or is_it_color(data_long[4:6],color_ref,color_error) or is_it_color(data_long[8:10],color_ref,color_error)):
             #Mark time the lastest particle passed by
-            particle_arrival = time.monotonic() XXXXThis needs to be a que/list/pop thingXXXX
+            particle_arrival.insert(0,time.monotonic())
     else:
         #The signal is below threshold, so it it's ready for particles...flag down
         #Moving average for long data
         for i in range(len(data_long)):
             data_long_base[i] = (1-base_weight) * data_long_base[i] + base_weight * data_long[i]
         #print(data_long_base)
-
 
     #Switches
     # In case of switch A: force basline
